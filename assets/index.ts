6ebd99ab34e3237b0cb69adde1141f875e782ea0
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-17 04:33:51 +0100

    feat: added google-icons

---
+++ assets/index.ts
@@ -0,0 +1,50 @@
+import bulb_icon from "./bulb_icon.png";
+import compass_icon from "./compass_icon.png";
+import user_icon from "./user_icon.png";
+import message_icon from "./message_icon.png";
+import code_icon from "./code_icon.png";
+import gemini_icon from "./gemini_icon.png";
+
+import add_photo from "./icons/add-photo.svg";
+import arrow_dropdown from "./icons/arrow-dropdown.svg";
+import darkmode from "./icons/dark-mode.svg";
+import extensions from "./icons/extension.svg";
+import google_apps from "./icons/google-apps.svg";
+import help from "./icons/help.svg";
+import history_icon from "./icons/history.svg";
+import link from "./icons/link.svg";
+import menu_icon from "./icons/menu-icon.svg";
+import mic_icon from "./icons/mic.svg";
+import plus_icon from "./icons/plus.svg";
+import settings_icon from "./icons/settings.svg";
+import send_icon from "./icons/send.svg";
+import flight from "./icons/flight.svg";
+import write from "./icons/write.svg";
+import lightbulb from "./icons/lightbulb.svg";
+import table_restaurant from "./icons/table_restaurant.svg";
+
+export const assets = {
+  add_photo,
+  arrow_dropdown,
+  darkmode,
+  extensions,
+  google_apps,
+  help,
+  link,
+  settings_icon,
+  history_icon,
+  menu_icon,
+  plus_icon,
+  bulb_icon,
+  compass_icon,
+  mic_icon,
+  user_icon,
+  message_icon,
+  code_icon,
+  send_icon,
+  gemini_icon,
+  flight,
+  write,
+  lightbulb,
+  table_restaurant,
+};
