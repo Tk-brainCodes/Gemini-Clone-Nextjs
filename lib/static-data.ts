7b8aebf8b3f049b80a0e5ec216447e338e7f14ad
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-18 13:03:55 +0100

    feat: static prompt card data

---
+++ lib/static-data.ts
@@ -0,0 +1,22 @@
+import { assets } from "@/assets";
+
+export const static_data = [
+  {
+    prompt:
+      "Show me flights to Tokyo and give me ideas of things to do. How about Seoul too?",
+    icon: assets.flight,
+  },
+  {
+    prompt: "Write a product description for a new type of toothbrush",
+    icon: assets.write,
+  },
+  {
+    prompt:
+      "Create a recipe for a low-carb meal with the following ingredients I have in my fridge: cauliflower, and cucumber",
+    icon: assets.table_restaurant,
+  },
+  {
+    prompt: "Explain what the keto diet is in simple terms",
+    icon: assets.lightbulb,
+  },
+];
