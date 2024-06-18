2048ae0e7112c09d12a06eb4930e296dc239028a
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-18 13:00:54 +0100

    feat: dynamic tooltip component

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface ActionTooltipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip  delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent className="bg-[#444746] text-white" align={align} side={side}>
+          <p className='font-normal text-sm capitalize'>
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionTooltip;
