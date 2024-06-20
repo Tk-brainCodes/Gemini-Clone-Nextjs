169f281e93654cf33a394a9f6da93c677d2a787c
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-20 21:20:07 +0100

    chore: removed currentUser import

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import SidebarDrawer from "@/components/sidebar/sidebar";
 import AITextGenerator from "@/components/main/main";
 
-import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user = await currentUser();
 
   return (
     <main className='flex flex-row'>
       <SidebarDrawer />
-      <AITextGenerator user={user?.id} />
+      <AITextGenerator />
     </main>
   );
 }
