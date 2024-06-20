358a060e0e7eca4be40ef954a700aa5d7e1e419e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-20 21:20:31 +0100

    feat: deleted clerks middleware

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className='flex items-center justify-center flex-col gap-10'>
-      <SignIn />
-    </div>
-  );
-}
