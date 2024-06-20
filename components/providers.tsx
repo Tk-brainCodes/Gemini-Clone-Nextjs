149104267ce8227e657c86f1f9f3ae078705c6ef
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-20 21:20:44 +0100

    feat: removed clerks provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { ClerkProvider } from "@clerk/nextjs";
 import store from "@/redux/store";
 
 type ProvidersProps = {
@@ -10,9 +9,7 @@ type ProvidersProps = {
 
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
   return (
-    <ClerkProvider>
       <Provider store={store}>{children}</Provider>
-    </ClerkProvider>
   );
 };
 
