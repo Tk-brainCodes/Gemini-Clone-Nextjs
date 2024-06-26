7274cf770fbfcc8edb2a0e9bbe710ed688feaf04
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-26 20:55:44 +0100

    feat: added notification toast for when code is copied

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import {
   setPrompt,
+  addUserMessage,
   fetchGeneratedText,
   selectPrompt,
-  selectGeneratedText,
+  selectMessages,
   selectStatus,
+  selectError,
 } from "@/redux/conversationSlice";
-
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Image from "next/image";
+import { assets } from "@/assets";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -17,32 +24,74 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 import ActionTooltip from "@/components/action-tooltip";
+import WelcomeScreen from "./welcome-screen";
 
-import Image from "next/image";
-import { assets } from "@/assets";
-import { static_data } from "@/lib/static-data";
-
-const AITextGenerator = ({ user }: { user: any }) => {
-  const [userId, setUserId] = useState<string>("");
-  const dispatch = useAppDispatch();
-  const prompt = useAppSelector(selectPrompt);
-  const generatedText = useAppSelector(selectGeneratedText);
-  const status = useAppSelector(selectStatus);
-
-    useEffect(() => {
-    if (!user) {
-      localStorage.setItem('userId', user);
-    }
-    setUserId(user);
-  }, [user]);
+const CodeBlock = ({
+  language,
+  value,
+}: {
+  language: string;
+  value: string;
+}) => {
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(value);
+    toast("Copied to clipboard");
+  };
 
   return (
-    <div className='w-full bg-white  h-[100vh] pb-[15vh] relative overflow-x-hidden'>
+    <div className='relative'>
+      <SyntaxHighlighter
+        className='px-4 py-4  bg-[#f0f4f9] rounded-t-lg rounded-tr-lg'
+        style={docco}
+        language={language}
+        PreTag='div'
+      >
+        {value}
+      </SyntaxHighlighter>
+      <div className='copy_content w-full mt-1 px-4 py-4 h-[40px] bg-[#f8f8ff] flex items-center justify-between'>
+        <div className='leading-[14px] text-[#444746]'>
+          Use code{" "}
+          <a className='text-underline text-[16px] text-[#0b57d0] cursor-pointer'>
+            with caution
+          </a>
+        </div>
+        <Image
+          src={assets.content_copy}
+          alt='cotent_copy'
+          onClick={handleCopyClick}
+          className='cursor-pointer'
+        />
+      </div>
+    </div>
+  );
+};
+
+const AITextGenerator: React.FC = () => {
+  const [showResult, setShowResult] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const prompt = useAppSelector(selectPrompt);
+  const messages = useAppSelector(selectMessages);
+  const status = useAppSelector(selectStatus);
+  const error = useAppSelector(selectError);
+
+  const handleGenerate = () => {
+    dispatch(addUserMessage(prompt));
+    dispatch(fetchGeneratedText({ prompt }));
+    setShowResult(true);
+  };
+
+  console.log("messages", messages);
+
+  return (
+    <div className='w-full bg-white h-screen pb-[15vh] relative overflow-x-hidden'>
+      <Toaster position='bottom-left' />
       <div className='flex items-center justify-between h-[40px] px-4 py-2 mt-5 relative right-0 w-full'>
         <DropdownMenu>
-          <DropdownMenuTrigger className='text-[20px] leading-[28px] text-[#5f6368] flex items-center justify-center gap-x-2 '>
-            <span className=''>Gemini</span>
+          <DropdownMenuTrigger className='text-[20px] leading-[28px] text-[#5f6368] flex items-center justify-center gap-x-2'>
+            <span>Gemini</span>
             <Image src={assets.arrow_dropdown} alt='arrow_dropdown' />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
@@ -51,11 +100,8 @@ const AITextGenerator = ({ user }: { user: any }) => {
           </DropdownMenuContent>
         </DropdownMenu>
 
-        <div className='flex gap-x-4 '>
-          <Button
-            variant='outline'
-            className='bg-[#dde3ea] text-[#000000] font-normal'
-          >
+        <div className='flex gap-x-4'>
+          <Button className='bg-[#dde3ea] text-[#000000] font-normal'>
             Try Gemini Advanced
           </Button>
           <ActionTooltip align='center' side='bottom' label='Google apps'>
@@ -64,85 +110,204 @@ const AITextGenerator = ({ user }: { user: any }) => {
             </Button>
           </ActionTooltip>
 
-          {/* <Image
-              src={assets.user_icon}
-              alt='user-profile-icon'
-              className='w-[40px] h-[40px] rounded-full'
-            /> */}
+          <Image
+            src={assets.user_icon}
+            alt='user_icon'
+            className='w-[40px] h-[40px] rounded-full cursor-pointer'
+          />
         </div>
       </div>
 
-     <section className="flex flex-col items-center justify-start  mt-[3em]"> 
-           {/*Card body*/}
-     
-     <div className="w-fit h-fit flex flex-col"> 
-         <span className='font-semibold mt-[2em]'>
-          <p className='gradient-text text-[56px] leading-[64px] text-[#c4c7c5]'>Hello, Eboreime</p>
-          <p className='text-[#c4c7c5] text-[56px] leading-[64px]'>How can I help you today?</p>
-        </span>
-        <div className='flex items-center justify-start gap-x-3 mt-[7em]'>
-          {static_data.map(
-            ({ prompt, icon }: { prompt: string; icon: string }) => (
-              <div
-                key={prompt}
-                className='h-[200px] w-[230px] p-[15px] bg-[#f0f4f9] rounded-[13px] relative cursor-pointer hover:bg-[#dfe4ed]'
-              >
-                <p className='text-black text-[17px] flex flex-col items-start '>
-                  {prompt}
-                </p>
-                <span className='w-[40px] h-[40px] p-[5px] bg-white rounded-full flex items-center justify-center right-[10px] bottom-[10px] absolute'>
-                  <Image src={icon} alt={`${icon}`} />
-                </span>
+      <section className='flex flex-col items-center justify-start'>
+        <div className='result w-[712px] mt-[2em]'>
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`my-10 w-full ${
+                message.sender === "ai" ? "flex-row-reverse" : ""
+              }`}
+            >
+              <div className='markdown-content w-[712px] flex flex-col items-start justify-start gap-[22px] text-[17px] font-normal text-base leading-[1.7] text-[#1f1f1f] group'>
+                <div className='flex items-center w-full group gap-[20px]'>
+                  { message.sender === "user" ? (
+                    <>
+                    <Image
+                      src={assets.user_icon}
+                      alt='user_icon'
+                      className='w-[36px] h-[36px] cursor-pointer rounded-full'
+                    />
+                    </>
+                  )
+                    : message.sender === "ai" ? (
+                 <>
+                      <Image
+                      src={assets.gemini_icon}
+                      alt='user_icon'
+                      className='w-[36px] h-[36px] hidden cursor-pointer rounded-full'
+                    />
+                 </>
+                    ) : ""
+}
+                  <div className='flex-1 mx-2'>
+                    {message.sender === "user" ? message.text : ""}
+                  </div>
+                  {message.sender === "user" && (
+                    <div className='hidden group-hover:block'>
+                      <Image
+                        src={assets.edit_icon}
+                        alt='edit_icon'
+                        className='w-[24px] cursor-pointer'
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <div className='flex items-start justify-start gap-[20px]'>
+                  <Image
+                    src={message.sender === "ai" ? assets.gemini_icon : ""}
+                    alt='user_icon'
+                    className='w-[32px] h-[32px] mt-[0.7em] cursor-pointer rounded-full'
+                  />
+                  {message.sender === "ai" && (
+                    <ReactMarkdown
+                      remarkPlugins={[remarkGfm]}
+                      className='w-full'
+                      components={{
+                        //@ts-ignore
+                        code({ node, inline, className, children, ...props }) {
+                          const match = /language-(\w+)/.exec(className || "");
+                          return !inline && match ? (
+                            <CodeBlock
+                              language={match[1]}
+                              value={String(children).replace(/\n$/, "")}
+                            />
+                          ) : (
+                            <code className={className} {...props}>
+                              {children}
+                            </code>
+                          );
+                        },
+                      }}
+                    >
+                      {message.text}
+                    </ReactMarkdown>
+                  )}
+                </div>
+
+                {message.sender === "user" && (
+                  <div className='w-full flex items-end justify-end gap-x-2 mt-2'>
+                    <Button className='bg-none text-[14px] text-[#1f1f1f] flex gap-x-2'>
+                      Show draft
+                      <Image
+                        src={assets.keyboard_arrow}
+                        alt='edit_icon'
+                        className='w-[24px] cursor-pointer'
+                      />
+                    </Button>
+                    <Button className='px-2 py-2 rounded-full'>
+                      <Image
+                        src={assets.volume_up}
+                        alt='edit_icon'
+                        className='w-[30px] h-[30px] cursor-pointer'
+                      />
+                    </Button>
+                  </div>
+                )}
               </div>
-            )
+            </div>
+          ))}
+
+          {status === "loading" && (
+            <div className='flex items-start gap-[20px]'>
+              <div className='w-[700px] flex flex-col gap-[10px]'>
+                <hr className='skeleton-loader' />
+                <hr className='skeleton-loader' />
+                <hr className='skeleton-loader w-[70%]' />
+              </div>
+            </div>
           )}
         </div>
-     </div>
 
-      {/*Search input container*/}
-      <div className=' background-gradient h-[130px] w-full flex flex-col fixed bottom-0 items-center justify-center bg-gradient-to-b from-transparent via-white to-white'>
-        <div className='search-box mt-[1em]'>
-          <textarea
-            placeholder='Enter a text here'
-            className='flex-1 h-[50px] w-[690px] bg-transparent border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 p-[8px] text-[18px]'
-            style={{ whiteSpace: "pre-wrap" }}
-          />
+        {showResult === false && status !== "succeeded" ? (
+          <WelcomeScreen />
+        ) : (
+          ""
+        )}
 
-          <div className='flex gap-x-2'>
-            <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
-              <Image
-                src={assets.add_photo}
-                alt='gallery-icon'
-                className='w-[24px] cursor-pointer'
+        <div className='background-gradient w-[80vw] flex flex-col fixed bottom-0 items-center justify-center'>
+          <div className='search-box mt-[1em]'>
+            <div className='relative flex items-center'>
+              <textarea
+                placeholder='Enter a text here'
+                className='flex-1 h-[50px] w-[690px] text-[#1f1f1f] bg-transparent border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 p-[8px] text-[18px]'
+                style={{ whiteSpace: "pre-wrap" }}
+                value={prompt}
+                onChange={(e) => dispatch(setPrompt(e.target.value))}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleGenerate();
+                  }
+                }}
               />
-            </span>
+            </div>
 
-            <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
-              <Image
-                src={assets.mic_icon}
-                alt='mic-icon'
-                className='w-[24px] cursor-pointer'
-              />
-            </span>
-            <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
-              <Image
-                src={assets.send_icon}
-                alt='send icon'
-                className='w-[24px] cursor-pointer '
-              />
-            </span>
+            <div className='flex gap-x-2 mt-2'>
+              <ActionTooltip align='center' side='top' label='Upload Image'>
+                <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+                  <Image
+                    src={assets.add_photo}
+                    alt='gallery-icon'
+                    className='w-[24px] cursor-pointer'
+                  />
+                </span>
+              </ActionTooltip>
+
+              <ActionTooltip align='center' side='top' label='Use microphone'>
+                <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+                  <Image
+                    src={assets.mic_icon}
+                    alt='mic-icon'
+                    className='w-[24px] cursor-pointer'
+                  />
+                </span>
+              </ActionTooltip>
+              {status === "loading" ? (
+                <ActionTooltip align='center' side='top' label='Stop'>
+                  <span className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+                    <Image
+                      src={assets.stop_icon}
+                      alt='stop icon'
+                      className='w-[24px] cursor-pointer'
+                    />
+                  </span>
+                </ActionTooltip>
+              ) : (
+                <ActionTooltip align='center' side='top' label='Submit'>
+                  <span
+                    className='rounded-full w-[50px] h-[50px] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'
+                    onClick={handleGenerate}
+                  >
+                    <Image
+                      src={assets.send_icon}
+                      alt='send icon'
+                      className='w-[24px] cursor-pointer'
+                    />
+                  </span>
+                </ActionTooltip>
+              )}
+            </div>
           </div>
-        </div>
 
-        <p className='mx-auto my-15px text-[14px] text-center text-black font-normal mb-1 mt-[1em]'>
-          Gemini may display inaccurate info, including about people, so
-          double-check its responses.{" "}
-          <span className='underline cursor-pointer'>
-            Your privacy & Gemini Apps
-          </span>
-        </p>
-      </div>
-     </section>
+          <p className='mx-auto my-15px text-[14px] text-center text-black font-normal mb-1 mt-[1em]'>
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses.{" "}
+            <span className='underline cursor-pointer'>
+              Your privacy & Gemini Apps
+            </span>
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
