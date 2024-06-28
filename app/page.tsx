454aa5c01de526c173429fde31bdd20147ead838
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-06-28 14:45:47 +0100

    feat: index page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import SidebarDrawer from "@/components/sidebar/sidebar";
 import AITextGenerator from "@/components/main/main";
 
-
 export default async function Home() {
-
   return (
     <main className='flex flex-row'>
       <SidebarDrawer />
