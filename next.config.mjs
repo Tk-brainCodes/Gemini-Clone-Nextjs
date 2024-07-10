06eb89172923d735371516291cd20bf7b09412d7
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-10 23:16:08 +0100

    feat: added cleerk image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    domains: ['img.clerk.com']
+  }
+}
+
+export default nextConfig
