3cd50671ac6dd836781c2f8b13bbcfd22c77a071
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-15 00:07:13 +0100

    feat: added createShortTitlePrompt for generating new prompt titles 🔥

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,70 +1,69 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import {
+  ChatPromptTemplate,
+  FewShotChatMessagePromptTemplate,
+} from "@langchain/core/prompts";
 
-// const createShortTitlePrompt = async (texts: string[]): Promise<string[]> => {
-//   return texts.map(
-//     (text) => `
-//     Please provide a short and concise title for the following text:
-    
-//     Text: "${text}"
-    
-//     Title:
-//   `
-//   );
-// };
+const model = new ChatGoogleGenerativeAI({
+  apiKey: `${process.env.GOOGLE_API_KEY}`,
+  temperature: 0.7,
+  model: "gemini-1.5-pro-latest",
+  maxOutputTokens: 8192,
+  topK: 64,
+  topP: 0.95,
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+  ],
+});
 
-// export const createDynamicTitle = async (
-//   prompts: string[]
-// ): Promise<string[]> => {
-//   const model = new ChatGoogleGenerativeAI({
-//     apiKey: `${process.env.GOOGLE_API_KEY}`,
-//     temperature: 0.7,
-//     model: "gemini-1.5-pro-latest",
-//     maxOutputTokens: 100,
-//     topK: 64,
-//     topP: 0.95,
-//     safetySettings: [
-//       {
-//         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-//         threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-//       },
-//     ],
-//   });
+export const createShortTitlePrompts = async (prompts: string[]) => {
+  const results = [];
 
-//   const responses = await Promise.all(
-//     prompts.map(async (prompt) => {
-//       const res = await model.invoke([["human", `${prompt}`]]);
+  const examples = [
+    {
+      input:
+        "in a tabular format, give me the difference between rust and javascript",
+      output: "Rust vs. Javascript",
+    },
+    {
+      input:
+        "write a code to Create a recipe for a low-carb meal with the following ingredients I have in my fridge: cauliflower, and cucumber in react",
+      output: "React Recipe Display",
+    },
+    {
+      input: "give me the history of algeria",
+      output: "A Brief History of Algeria",
+    },
+  ];
 
-//       if (Array.isArray(res)) {
-//         return res
-//           .map((r) => r.content)
-//           .join(" ")
-//           .trim();
-//       }
-
-//       return (res.content as string).trim();
-//     })
-//   );
-
-//   return responses;
-// };
-
-export const createDynamicPromptInput = async (prompt: string) => {
-  const model = new ChatGoogleGenerativeAI({
-    apiKey: `${process.env.GOOGLE_API_KEY}`,
-    temperature: 0.7,
-    model: "gemini-1.5-pro-latest",
-    maxOutputTokens: 8192,
-    topK: 64,
-    topP: 0.95,
-    safetySettings: [
-      {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-      },
-    ],
+  const examplePrompt = ChatPromptTemplate.fromTemplate(`Human: {input}
+{output}`);
+  const fewShotPrompt = new FewShotChatMessagePromptTemplate({
+    prefix:
+      "Rephrase the users query to be more general, using the following examples",
+    suffix: "Human: {input}",
+    examplePrompt,
+    examples,
+    inputVariables: ["input"],
   });
 
+  for (const prompt of prompts) {
+    const formattedPrompt = await fewShotPrompt.format({
+      input: prompt,
+    });
+
+    const responses = await model.invoke(formattedPrompt);
+    results.push(responses.content);
+  }
+
+  return results;
+};
+
+export const createDynamicPromptInput = async (prompt: string) => {
   const res = await model.invoke([["human", `${prompt}`]]);
   return res.content;
 };
