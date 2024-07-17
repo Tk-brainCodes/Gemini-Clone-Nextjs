d4dabc958daf07f5175cd240717c9ec5a1c372e1
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-17 22:18:44 +0100

    chore: updated next config with configuration for svg images using webpack

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,24 @@
 const nextConfig = {
   images: {
     domains: ['img.clerk.com']
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+            titleProp: true,
+            ref: true
+          }
+        },
+        'url-loader'
+      ]
+    })
+
+    return config
   }
 }
 
