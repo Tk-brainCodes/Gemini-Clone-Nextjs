484cfb32e48c41c297f907a80e7ac1848aca55a0
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-22 22:13:11 +0100

    chore: code cleanup

--- app/api/get-title/route.ts
+++ app/api/get-title/route.ts
@@ -6,11 +6,14 @@ export async function POST(req: Request, res: NextApiResponse) {
   try {
     const { prompts } = await req.json();
 
+    console.log("received prompts on the backend", prompts);
+
     if (!prompts || !Array.isArray(prompts)) {
       return res.status(400).json({ error: "Invalid prompts array" });
     }
 
     const titles = await createShortTitlePrompts(prompts);
+    console.log("titles from back end", titles);
 
     return NextResponse.json({ titles });
   } catch (error) {
