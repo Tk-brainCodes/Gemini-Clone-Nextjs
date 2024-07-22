469859d307616632ffe093b23e7d57c43ff5c9c5
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-22 22:13:39 +0100

    feat: added new imports for svg icons

--- assets/index.ts
+++ assets/index.ts
@@ -1,65 +1,86 @@
-// import gemini_icon from "./gemini_icon.png";
+import { ReactComponent as AddPhotoIcon } from "./icons/add-photo.svg";
+import { ReactComponent as ArrowDropdownIcon } from "./icons/arrow-dropdown.svg";
+import { ReactComponent as DarkModeIcon } from "./icons/dark-mode.svg";
+import { ReactComponent as ExtensionsIcon } from "./icons/extension.svg";
+import { ReactComponent as GoogleAppsIcon } from "./icons/google-apps.svg";
+import { ReactComponent as HelpIcon } from "./icons/help.svg";
+import { ReactComponent as HistoryIcon } from "./icons/history.svg";
+import { ReactComponent as LinkIcon } from "./icons/link.svg";
+import { ReactComponent as MenuIcon } from "./icons/menu-icon.svg";
+import { ReactComponent as MicIcon } from "./icons/mic.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
+import { ReactComponent as SendIcon } from "./icons/send.svg";
+import { ReactComponent as FlightIcon } from "./icons/flight.svg";
+import { ReactComponent as WriteIcon } from "./icons/write.svg";
+import { ReactComponent as LightbulbIcon } from "./icons/lightbulb.svg";
+import { ReactComponent as TableRestaurantIcon } from "./icons/table_restaurant.svg";
+import { ReactComponent as StopIcon } from "./icons/stop_circle.svg";
+import { ReactComponent as EditIcon } from "./icons/edit.svg";
+import { ReactComponent as ContentCopyIcon } from "./icons/content_copy.svg";
+import { ReactComponent as VolumeUpIcon } from "./icons/volume_up.svg";
+import { ReactComponent as KeyboardArrowIcon } from "./icons/keyboard_arrow.svg";
+import { ReactComponent as ChatBubbleIcon } from "./icons/chat_bubble.svg";
+import { ReactComponent as OptionIcon } from "./icons/option.svg";
+import { ReactComponent as PauseIcon } from "./icons/pause.svg";
+import { ReactComponent as StopIconAlias } from "./icons/stop.svg";
+import { ReactComponent as ProgressActivityIcon } from "./icons/progress_activity.svg";
+import { ReactComponent as ThemeModeIcon } from "./icons/dark-mode.svg";
+import { ReactComponent as Delete } from "./icons/delete.svg";
+import { ReactComponent as Pin } from "./icons/keep.svg";
+import { ReactComponent as Unpin } from "./icons/keep_off.svg";
+import { ReactComponent as Emoji } from "./icons/mood.svg";
+import { ReactComponent as Tune } from "./icons/tune.svg";
+import { ReactComponent as GoogleBrand } from "./icons/google-brand.svg";
+import { ReactComponent as ThumbsUp } from "./icons/thumb_up.svg";
+import { ReactComponent as ThumbsDown } from "./icons/thumb_down.svg";
+import { ReactComponent as Share } from "./icons/share.svg";
+import { ReactComponent as Flag } from "./icons/flag.svg";
 
-import add_photo from "./icons/add-photo.svg";
-import arrow_dropdown from "./icons/arrow-dropdown.svg";
-import darkmode from "./icons/dark-mode.svg";
-import extensions from "./icons/extension.svg";
-import google_apps from "./icons/google-apps.svg";
-import help from "./icons/help.svg";
-import history_icon from "./icons/history.svg";
-import link from "./icons/link.svg";
-import menu_icon from "./icons/menu-icon.svg";
-import mic_icon from "./icons/mic.svg";
-import plus_icon from "./icons/plus.svg";
-import settings_icon from "./icons/settings.svg";
-import send_icon from "./icons/send.svg";
-import flight from "./icons/flight.svg";
-import write from "./icons/write.svg";
-import lightbulb from "./icons/lightbulb.svg";
-import table_restaurant from "./icons/table_restaurant.svg";
-import stop_icon from "./icons/stop_circle.svg";
-import user_icon from "./icons/user_icon.png";
+import UserIcon from "./icons/user_icon.png";
 import gemini_icon from "./icons/gemini_icon.png";
-import edit_icon from "./icons/edit.svg";
-import content_copy from "./icons/content_copy.svg";
-import volume_up from "./icons/volume_up.svg";
-import keyboard_arrow from "./icons/keyboard_arrow.svg";
-import chat_bubble from "./icons/chat_bubble.svg";
-import option from "./icons/option.svg";
-import gemini_sparkel from "./icons/gemini_sparkle.svg";
-import pause from "./icons/pause.svg";
-import stop from "./icons/stop.svg";
-import progress_activity from "./icons/progress_activity.svg";
+import gemini_sparkle from "./icons/gemini_sparkle.svg";
 
 export const assets = {
-  add_photo,
-  arrow_dropdown,
-  darkmode,
-  extensions,
-  google_apps,
-  help,
-  link,
-  settings_icon,
-  history_icon,
-  menu_icon,
-  plus_icon,
-  mic_icon,
-  send_icon,
-  flight,
-  write,
-  lightbulb,
-  table_restaurant,
-  stop_icon,
-  user_icon,
+  AddPhotoIcon,
+  ArrowDropdownIcon,
+  DarkModeIcon,
+  ExtensionsIcon,
+  GoogleAppsIcon,
+  HelpIcon,
+  HistoryIcon,
+  LinkIcon,
+  MenuIcon,
+  MicIcon,
+  PlusIcon,
+  SettingsIcon,
+  SendIcon,
+  FlightIcon,
+  WriteIcon,
+  LightbulbIcon,
+  TableRestaurantIcon,
+  StopIcon,
+  UserIcon,
   gemini_icon,
-  edit_icon,
-  content_copy,
-  volume_up,
-  keyboard_arrow,
-  chat_bubble,
-  option,
-  gemini_sparkel,
-  stop,
-  pause,
-  progress_activity,
+  EditIcon,
+  ContentCopyIcon,
+  VolumeUpIcon,
+  KeyboardArrowIcon,
+  ChatBubbleIcon,
+  OptionIcon,
+  gemini_sparkle,
+  PauseIcon,
+  StopIconAlias,
+  ProgressActivityIcon,
+  ThemeModeIcon,
+  Delete,
+  Pin,
+  Unpin,
+  Emoji,
+  Share,
+  ThumbsDown,
+  ThumbsUp,
+  GoogleBrand,
+  Tune,
+  Flag,
 };
