4944f7d764eb59121f16ea17c152dd224763d401
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-23 22:07:03 +0100

    chore: update icon import

--- lib/static-data.ts
+++ lib/static-data.ts
@@ -1,22 +1,23 @@
 import { assets } from "@/assets";
+import { StaticDataItem } from "@/types/static-card-types";
 
-export const static_data = [
+export const static_data: StaticDataItem[] = [
   {
     prompt:
       "Show me flights to Tokyo and give me ideas of things to do. How about Seoul too?",
-    icon: assets.flight,
+    icon: assets.FlightIcon,
   },
   {
     prompt: "Write a product description for a new type of toothbrush",
-    icon: assets.write,
+    icon: assets.WriteIcon,
   },
   {
     prompt:
       "Create a recipe for a low-carb meal with the following ingredients I have in my fridge: cauliflower, and cucumber",
-    icon: assets.table_restaurant,
+    icon: assets.TableRestaurantIcon,
   },
   {
     prompt: "Explain what the keto diet is in simple terms",
-    icon: assets.lightbulb,
+    icon: assets.LightbulbIcon,
   },
 ];
