a501360291cc2318aff1656dc936554caed244ae
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-26 16:00:19 +0100

    feat: added displaying of send icon when the input field is active

--- components/prompt-input/prompt-input.tsx
+++ components/prompt-input/prompt-input.tsx
@@ -23,6 +23,7 @@ const PromptInput = () => {
     useSpeechToText();
   const [inputText, setInputText] = useState<string>(transcript);
   const [isTalking, setIsTalking] = useState(false);
+  const [inputActive, setInputActive] = useState(false);
   const dispatch = useAppDispatch();
   const router = useRouter();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -43,29 +44,60 @@ const PromptInput = () => {
     }
   }, [isListening]);
 
+  // const handleGenerate = async () => {
+  //   if (!inputText.trim()) return;
+
+  //   if (currentSession?.id) {
+  //     dispatch(
+  //       addUserMessage({ sessionId: currentSession.id, text: inputText })
+  //     );
+  //     await dispatch(sendUserPropmtToAI({ prompt: inputText }));
+  //     await dispatch(saveChatSession());
+  //     setInputText("");
+  //     router.push(`/chat/${currentSession.id}`);
+  //   } else {
+  //     await dispatch(startNewSession());
+  //     const newSessionId = currentSessionId;
+
+  //     if (newSessionId) {
+  //       dispatch(setCurrentSession(newSessionId));
+  //       dispatch(addUserMessage({ sessionId: newSessionId, text: inputText }));
+  //       await dispatch(sendUserPropmtToAI({ prompt: inputText }));
+  //       await dispatch(saveChatSession());
+  //       router.push(`/chat/${newSessionId}`);
+  //       setInputText("");
+  //     }
+  //   }
+  // };
+
+  const handleFocus = () => {
+    setInputActive(true);
+  };
+
   const handleGenerate = async () => {
     if (!inputText.trim()) return;
 
-    if (currentSession?.id) {
-      dispatch(
-        addUserMessage({ sessionId: currentSession.id, text: inputText })
-      );
-      await dispatch(sendUserPropmtToAI({ prompt: inputText }));
-      await dispatch(saveChatSession());
-      setInputText("");
-      router.push(`/chat/${currentSession.id}`);
-    } else {
-      await dispatch(startNewSession());
-      const newSessionId = currentSessionId;
+    let sessionId = currentSession?.id ?? null;
 
-      if (newSessionId) {
-        dispatch(setCurrentSession(newSessionId));
-        dispatch(addUserMessage({ sessionId: newSessionId, text: inputText }));
-        await dispatch(sendUserPropmtToAI({ prompt: inputText }));
-        await dispatch(saveChatSession());
-        router.push(`/chat/${newSessionId}`);
-        setInputText("");
-      }
+    if (!sessionId) {
+      await dispatch(startNewSession());
+      sessionId = currentSessionId ?? null;
+      if (!sessionId) return;
+      dispatch(setCurrentSession(sessionId));
+    }
+
+    const safeSessionId: string = sessionId;
+
+    dispatch(addUserMessage({ sessionId: safeSessionId, text: inputText }));
+    await dispatch(sendUserPropmtToAI({ prompt: inputText }));
+    await dispatch(saveChatSession());
+    setInputText("");
+    router.push(`/chat/${safeSessionId}`);
+  };
+
+  const handleBlur = () => {
+    if (inputText.trim() === "") {
+      setInputActive(false);
     }
   };
 
@@ -78,82 +110,92 @@ const PromptInput = () => {
   }, [inputText]);
 
   return (
-    <div className='h-fit bg-white dark:bg-[#131314] px-4 z-10 w-[75vw] max-sm:w-full max-md:w-full flex flex-col fixed bottom-0 items-center justify-center'>
+    <div className='h-fit bg-white dark:bg-[#131314] px-4 z-10 w-[76vw] max-sm:w-full max-md:w-full flex flex-col fixed bottom-0 items-center justify-center'>
       <div className='search-box dark:bg-[#1e1f20] lg:w-[890px] md:w-[763px] max-sm:w-[90vw] px-4'>
-          <textarea
-            ref={textareaRef}
-            placeholder={isTalking ? "Listening..." : "Enter a text here"}
-            className='flex-1 dark:text-[#c4c7c5] ml-[1em] p-0 w-full md:w-[660px] bg-transparent mt-[1em] text-[#1f1f1f] border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 text-[18px] resize-none overflow-auto max-h-[250px] min-h-[30px]'
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                handleGenerate();
-              }
-            }}
-          />
+        <textarea
+          ref={textareaRef}
+          placeholder={isTalking ? "Listening..." : "Enter a text here"}
+          className='flex-1 dark:text-[#c4c7c5] ml-[1em] p-0 w-full md:w-[660px] bg-transparent mt-[1em] text-[#1f1f1f] border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 text-[18px] resize-none overflow-auto max-h-[250px] min-h-[30px]'
+          value={inputText}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              handleGenerate();
+            }
+          }}
+        />
 
-          <div className='flex gap-x-2  items-center'>
-            <ActionTooltip align='center' side='top' label='Upload Image'>
-              <span className='rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
-                <assets.AddPhotoIcon
-                  width={24}
-                  height={24}
-                  className='w-[24px] fill-black dark:fill-white cursor-pointer'
-                />
-              </span>
-            </ActionTooltip>
+        <div className='flex gap-x-2  items-center'>
+          <ActionTooltip align='center' side='top' label='Upload Image'>
+            <span className='rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+              <assets.AddPhotoIcon
+                width={24}
+                height={24}
+                className='w-[24px] fill-black dark:fill-white cursor-pointer'
+              />
+            </span>
+          </ActionTooltip>
 
-            <ActionTooltip align='center' side='top' label='Use microphone'>
+          <ActionTooltip align='center' side='top' label='Use microphone'>
+            <span
+              onClick={isListening ? stopListening : startListening}
+              className={cn(
+                "rounded-full w-[50px] h-[50px] hover:bg-slate-200 dark:hover:bg-[#37393b] px-2 py-2 flex items-center justify-center",
+                isListening && "bg-[#f0f4f9] dark:bg-[#e3e3e3] shadow-md"
+              )}
+            >
+              {isListening && (
+                <span className='absolute w-[65px] h-[63px] rounded-full bg-[#37393b] opacity-55 motion-safe:animate-ping'></span>
+              )}
+              <assets.MicIcon
+                width={24}
+                height={24}
+                className='w-[24px] fill-black dark:fill-white cursor-pointer'
+              />
+            </span>
+          </ActionTooltip>
+          {status === "loading" ? (
+            <ActionTooltip align='center' side='top' label='Stop'>
               <span
-                onClick={isListening ? stopListening : startListening}
                 className={cn(
-                  "rounded-full w-[50px] h-[50px] hover:bg-slate-200 dark:hover:bg-[#37393b] px-2 py-2 flex items-center justify-center",
-                  isListening && "bg-[#f0f4f9] dark:bg-[#e3e3e3] shadow-md"
+                  "rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center dark:bg-[#37393b] bg-[#f0f4f9]"
                 )}
               >
-                {isListening && (
-                  <span className='absolute w-[65px] h-[63px] rounded-full bg-[#37393b] opacity-55 motion-safe:animate-ping'></span>
-                )}
-                <assets.MicIcon
+                <assets.StopIcon
                   width={24}
                   height={24}
                   className='w-[24px] fill-black dark:fill-white cursor-pointer'
                 />
               </span>
             </ActionTooltip>
-            {status === "loading" ? (
-              <ActionTooltip align='center' side='top' label='Stop'>
-                <span
+          ) : (
+            <ActionTooltip align='center' side='top' label='Submit'>
+              <span
+                className={cn(
+                  "rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2  transition-transform duration-300 ease-in-out",
+                  {
+                    "opacity-100 transform translate-x-0 flex items-center justify-center":
+                      inputActive,
+                    "opacity-0 transform translate-x-full absolute":
+                      !inputActive,
+                  }
+                )}
+                onClick={handleGenerate}
+              >
+                <assets.SendIcon
+                  width='24px'
+                  height='24px'
                   className={cn(
-                    "rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center dark:bg-[#37393b] bg-[#f0f4f9]"
+                    "w-[24px] fill-black cursor-pointer dark:fill-white"
                   )}
-                >
-                  <assets.StopIcon
-                    width={24}
-                    height={24}
-                    className='w-[24px] fill-black dark:fill-white cursor-pointer'
-                  />
-                </span>
-              </ActionTooltip>
-            ) : (
-              <ActionTooltip align='center' side='top' label='Submit'>
-                <span
-                  className='rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'
-                  onClick={handleGenerate}
-                >
-                  <assets.SendIcon
-                    width='24px'
-                    height='24px'
-                    className={cn(
-                      "w-[24px] fill-black cursor-pointer dark:fill-white"
-                    )}
-                  />
-                </span>
-              </ActionTooltip>
-            )}
-          </div>
+                />
+              </span>
+            </ActionTooltip>
+          )}
+        </div>
       </div>
 
       <p className='mx-auto my-15px text-[14px] text-center dark:text-[#c4c7c5] text-black font-normal mt-[0.5em] mb-[0.5em] gap-x-2'>
