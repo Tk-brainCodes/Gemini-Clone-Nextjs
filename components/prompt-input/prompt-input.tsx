8f6b7fa2dfaed087ceb3d838b29d87ed2b9a5786
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-08-02 22:17:06 +0100

    feat: added image selection into the gemini input

--- components/prompt-input/prompt-input.tsx
+++ components/prompt-input/prompt-input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, ChangeEvent } from "react";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useRouter } from "next/navigation";
 import {
@@ -24,9 +24,12 @@ const PromptInput = () => {
   const [inputText, setInputText] = useState<string>(transcript);
   const [isTalking, setIsTalking] = useState(false);
   const [inputActive, setInputActive] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
   const dispatch = useAppDispatch();
   const router = useRouter();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [isHovered, setIsHovered] = useState(false);
 
   const currentSession = useAppSelector(selectCurrentSession);
   const currentSessionId = useAppSelector(selectCurrentSessionId);
@@ -44,55 +47,71 @@ const PromptInput = () => {
     }
   }, [isListening]);
 
-  // const handleGenerate = async () => {
-  //   if (!inputText.trim()) return;
-
-  //   if (currentSession?.id) {
-  //     dispatch(
-  //       addUserMessage({ sessionId: currentSession.id, text: inputText })
-  //     );
-  //     await dispatch(sendUserPropmtToAI({ prompt: inputText }));
-  //     await dispatch(saveChatSession());
-  //     setInputText("");
-  //     router.push(`/chat/${currentSession.id}`);
-  //   } else {
-  //     await dispatch(startNewSession());
-  //     const newSessionId = currentSessionId;
-
-  //     if (newSessionId) {
-  //       dispatch(setCurrentSession(newSessionId));
-  //       dispatch(addUserMessage({ sessionId: newSessionId, text: inputText }));
-  //       await dispatch(sendUserPropmtToAI({ prompt: inputText }));
-  //       await dispatch(saveChatSession());
-  //       router.push(`/chat/${newSessionId}`);
-  //       setInputText("");
-  //     }
-  //   }
-  // };
-
-  const handleFocus = () => {
-    setInputActive(true);
-  };
-
   const handleGenerate = async () => {
     if (!inputText.trim()) return;
 
-    let sessionId = currentSession?.id ?? null;
-
-    if (!sessionId) {
+    if (currentSession?.id) {
+      dispatch(
+        addUserMessage({ sessionId: currentSession.id, text: inputText })
+      );
+      if (image) {
+        await dispatch(
+          sendUserPropmtToAI({
+            prompt: inputText,
+            image,
+          })
+        );
+      } else {
+        await dispatch(sendUserPropmtToAI({ prompt: inputText }));
+        setImage(null);
+      }
+      await dispatch(saveChatSession());
+      setInputText("");
+      setImage(null);
+      router.push(`/chat/${currentSession.id}`);
+    } else {
       await dispatch(startNewSession());
-      sessionId = currentSessionId ?? null;
-      if (!sessionId) return;
-      dispatch(setCurrentSession(sessionId));
+      const newSessionId = currentSessionId;
+
+      if (newSessionId) {
+        dispatch(setCurrentSession(newSessionId));
+        dispatch(addUserMessage({ sessionId: newSessionId, text: inputText }));
+        if (image) {
+          await dispatch(
+            sendUserPropmtToAI({
+              prompt: inputText,
+              image,
+            })
+          );
+        } else {
+          await dispatch(sendUserPropmtToAI({ prompt: inputText }));
+          setImage(null);
+        }
+        await dispatch(saveChatSession());
+        router.push(`/chat/${newSessionId}`);
+        setInputText("");
+        setImage(null);
+      }
     }
+  };
 
-    const safeSessionId: string = sessionId;
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-    dispatch(addUserMessage({ sessionId: safeSessionId, text: inputText }));
-    await dispatch(sendUserPropmtToAI({ prompt: inputText }));
-    await dispatch(saveChatSession());
-    setInputText("");
-    router.push(`/chat/${safeSessionId}`);
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFocus = () => {
+    setInputActive(true);
   };
 
   const handleBlur = () => {
@@ -112,25 +131,68 @@ const PromptInput = () => {
   return (
     <div className='h-fit bg-white dark:bg-[#131314] px-4 z-10 w-[76vw] max-sm:w-full max-md:w-full flex flex-col fixed bottom-0 items-center justify-center'>
       <div className='search-box dark:bg-[#1e1f20] lg:w-[890px] md:w-[763px] max-sm:w-[90vw] px-4'>
-        <textarea
-          ref={textareaRef}
-          placeholder={isTalking ? "Listening..." : "Enter a text here"}
-          className='flex-1 dark:text-[#c4c7c5] ml-[1em] p-0 w-full md:w-[660px] bg-transparent mt-[1em] text-[#1f1f1f] border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 text-[18px] resize-none overflow-auto max-h-[250px] min-h-[30px]'
-          value={inputText}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onChange={(e) => setInputText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && !e.shiftKey) {
-              e.preventDefault();
-              handleGenerate();
-            }
-          }}
-        />
+        <div className='flex flex-col gap-x-2'>
+          <textarea
+            ref={textareaRef}
+            placeholder={isTalking ? "Listening..." : "Enter a text here"}
+            className='flex-1 dark:text-[#c4c7c5] ml-[1em] p-0 w-full md:w-[660px] bg-transparent mt-[1em] text-[#1f1f1f] border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 text-[18px] resize-none overflow-auto max-h-[250px] min-h-[30px]'
+            value={inputText}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                handleGenerate();
+              }
+            }}
+          />
+          {image ? (
+            <span
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              className='w-fit ml-5 -mt-[1em] mb-[1em] relative flex  items-center justify-center  hover:opacity-50 transition-all ease-in-out
+'
+            >
+              <Image
+                src={image}
+                alt='Selected'
+                className='max-w-[100px] max-h-[100px] w-[50px] h-[50px] rounded-[10px]'
+                loading='lazy'
+                width={500}
+                height={500}
+              />
 
+              {isHovered && (
+                <assets.Close
+                  className='fill-white w-[24px] cursor-pointer absolute'
+                  width={24}
+                  height={24}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setImage(null);
+                  }}
+                />
+              )}
+            </span>
+          ) : (
+            ""
+          )}
+
+          <input
+            type='file'
+            accept='image/*'
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={handleImageChange}
+          />
+        </div>
         <div className='flex gap-x-2  items-center'>
           <ActionTooltip align='center' side='top' label='Upload Image'>
-            <span className='rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+            <span
+              className='rounded-full w-[50px] h-[50px] dark:hover:bg-[#37393b] hover:bg-slate-200 px-2 py-2 flex items-center justify-center'
+              onClick={handleButtonClick}
+            >
               <assets.AddPhotoIcon
                 width={24}
                 height={24}
